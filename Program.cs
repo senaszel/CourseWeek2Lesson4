d00c54d769a5901b3020f9cb4f7f00aef6734bfb
Marek Stanisław Meyer <marekstanislawmeyer@gmail.com>
2020-08-07 19:03:14 +0200

    fourth exercise

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace CourseWeek2Lesson4
 
         public static void Exercise1()
         {
-            /* 1.Stwórz program, w którym zadeklarujesz zmienne dotyczące danych pracownika firmy. Dane które chcesz przetrzymywać to:
+            /* 1. Stwórz program, w którym zadeklarujesz zmienne dotyczące danych pracownika firmy. Dane które chcesz przetrzymywać to:
                 a.Imię,
                 b.Nazwisko
                 c.Wiek
@@ -41,7 +41,7 @@ namespace CourseWeek2Lesson4
 
         public static double Exercise3(double _length,double _width)
         {
-            /* 3.Napisz program, który na podstawie podanej szerokości i długości prostokąta wyliczy długość przekątnej. (Aby, obliczyć kwadrat liczby użyj metody Math.Pow()) */
+            /* 3. Napisz program, który na podstawie podanej szerokości i długości prostokąta wyliczy długość przekątnej. (Aby, obliczyć kwadrat liczby użyj metody Math.Pow()) */
             double length = _length;
             double width = _width;
 
@@ -50,6 +50,20 @@ namespace CourseWeek2Lesson4
             Console.WriteLine($"Diagonal of Rectangle which length is {length} and width is {width} equals = {diagonal}");
             return diagonal;
         }
+
+        public static void Exercise4()
+        {
+            /* 4. Napisz program w którym stworzysz 2 zmienne liczbowe, oraz jedną tekstową, a następnie przypiszesz im następujące wartości:
+                a. 10
+                b. Szkoła Dotneta
+                c. 12,5
+            Pamiętaj o użyciu poprawnych typów danych. */
+
+            int ten = 10;
+            float twelveAndHalf = 12.5f;
+            string dotNetSchool = "Szkoła Dotneta";
+        }
+
 
     }
 }
